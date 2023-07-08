f71c2c769ffb397d9009262fccea6960eaa35bda
Deandre Baker <49622234+deandrebaker@users.noreply.github.com>
2023-07-07 20:48:27 -0700

    Setup browser extension

--- browser-extension/react-chrome-app/src/App.tsx
+++ browser-extension/react-chrome-app/src/App.tsx
@@ -1,24 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="browser container" style={{height: '550px', width: '450px', backgroundColor: 'gainsboro'}}>
+      <Header />
     </div>
   );
 }
---
+++ browser-extension/react-chrome-app/src/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const Header = () => {
+    return (
+        <div>
+            this is header
+        </div>
+    )
+}
+
+export default Header;
